1ff7feccf56b2c2b9cbc68d55e8b2942b0eb7b6f
Aneal07 <anilmaharjan07@outlook.com>
2024-07-04 11:54:16 +0100

    changed the project

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import WorkImg from '../assets/work.jpg';
-import CovidImg from '../assets/covid-stats.jpg';
-import GymGenius from '../assets/gymgenius.png'
+import React from 'react'
+import WorkImg from '../assets/work.jpg'
+import CovidImg from '../assets/covid-stats.jpg'
+import Estate from '../assets/estate.png'
 
 const Work = () => {
   return (
     <div id='work' className='w-full md:h-screen text-gray-300 bg-[#091425]'>
-      
       {/* Main container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center  w-full h-full'>
-        
         {/* Header */}
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-red-600'>
@@ -20,67 +18,112 @@ const Work = () => {
 
         {/* Projects Grid */}
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-
           {/* Project 1 */}
-          <div 
-            style={{backgroundImage: `url(${GymGenius})`}}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-            
+          <div
+            style={{ backgroundImage: `url(${Estate})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
             {/* hover effect */}
             <div className='opacity-0 group-hover:opacity-100'>
-              <h1 className='text-2xl font-bold m-2 text-center  border-b-2 text-gray-300 border-red-600'>Gym-Genius</h1>
+              <h1 className='text-2xl font-bold m-2 text-center  border-b-2 text-gray-300 border-red-600'>
+                MERN-Estate
+              </h1>
               <p className='m-2 text-white tracking-wider'>
-                GymGenius is a web application that allows users to search for exercises
-                 based on specific criteria such as cardio, upper body, and more. 
-                </p>
+                a full-stack web application for managing real estate
+                data.Tailwind css and leverage Insomnia for testing API
+                endpoints.
+              </p>
               <div className='pt-8 text-center'>
-                <a href="https://reactfitnessappp.netlify.app/" target='_blank' rel="noopener noreferrer">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Live</button>
+                <a
+                  href='https://mern-estate-o42x.onrender.com/'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Live
+                  </button>
                 </a>
-                <a href="https://github.com/Aneal07/Gym-Genius" target='_blank' rel="noopener noreferrer">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                <a
+                  href='https://github.com/Aneal07/MERN-Estate-Project'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
                 </a>
               </div>
             </div>
           </div>
           {/* Project 2 */}
-          <div 
-            style={{backgroundImage: `url(${CovidImg})`}}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-            
+          <div
+            style={{ backgroundImage: `url(${CovidImg})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
             {/* hover effect */}
             <div className='opacity-0 group-hover:opacity-100'>
-              <h1 className='text-2xl font-bold m-2 text-center  border-b-2 text-gray-300 border-red-600'>Covid-Stats</h1>
+              <h1 className='text-2xl font-bold m-2 text-center  border-b-2 text-gray-300 border-red-600'>
+                Covid-Stats
+              </h1>
               <p className='m-2  text-white tracking-wider'>
-                Covid-Stats Covid-Stats is a simple web application that provides statistics and data visualization for COVID-19 cases worldwide.
-                </p>
+                Covid-Stats Covid-Stats is a simple web application that
+                provides statistics and data visualization for COVID-19 cases
+                worldwide.
+              </p>
               <div className='pt-8 text-center'>
-                <a href="https://aneal07.github.io/Covid-Stats/" target='_blank' rel="noopener noreferrer">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Live</button>
+                <a
+                  href='https://aneal07.github.io/Covid-Stats/'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Live
+                  </button>
                 </a>
-                <a href="https://github.com/Aneal07/Covid-Stats" target='_blank' rel="noopener noreferrer">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                <a
+                  href='https://github.com/Aneal07/Covid-Stats'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
                 </a>
               </div>
             </div>
           </div>
           {/* Project 3 */}
-          <div 
-            style={{backgroundImage: `url(${WorkImg})`}}
-            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-            
+          <div
+            style={{ backgroundImage: `url(${WorkImg})` }}
+            className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
+          >
             {/* hover effect */}
             <div className='opacity-0 group-hover:opacity-100'>
-              <h1 className='text-2xl font-bold m-2 text-center  border-b-2 text-gray-300 border-red-600'>Bitebound</h1>
+              <h1 className='text-2xl font-bold m-2 text-center  border-b-2 text-gray-300 border-red-600'>
+                Bitebound
+              </h1>
               <p className='m-2 text-white tracking-wider'>
-                Bitebound is a web application build in react and designed for food that allows you to search by menu and price.
-                </p>
+                Bitebound is a web application build in react and designed for
+                food that allows you to search by menu and price.
+              </p>
               <div className='pt-8 text-center'>
-                <a href="https://aneal07.github.io/Restaurent-Website/" target='_blank' rel="noopener noreferrer">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Live</button>
+                <a
+                  href='https://aneal07.github.io/Restaurent-Website/'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Live
+                  </button>
                 </a>
-                <a href="https://github.com/Aneal07/Restaurent-Website" target='_blank' rel="noopener noreferrer">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                <a
+                  href='https://github.com/Aneal07/Restaurent-Website'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                    Code
+                  </button>
                 </a>
               </div>
             </div>
@@ -91,4 +134,4 @@ const Work = () => {
   )
 }
 
-export default Work;
+export default Work
