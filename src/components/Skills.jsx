53db06511582a3241f8ff938c72d87a2b9f1dd1e
Aneal07 <anilmaharjan07@outlook.com>
2024-08-02 10:05:22 +0100

    skill update insomnia & pic

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,83 +14,94 @@ import Node from '../assets/node.png'
 import Express from '../assets/express.jpg'
 import Mongo from '../assets/mongodb.webp'
 import Postman from '../assets/postman.png'
-
+import Insomia from '../assets/insomnia.png'
 
 const Skills = () => {
   return (
     <div name='skills' className='w-full h-full bg-[#0a192f] text-gray-300'>
       {/* Container */}
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center  w-full h-full'>
-          <div>
-              <p className='text-4xl font-bold inline border-b-4 border-red-600 '>Tech-Stack</p>
-              <p className='py-4'>These are the technologies I've worked with</p>
+        <div>
+          <p className='text-4xl font-bold inline border-b-4 border-red-600 '>
+            Tech-Stack
+          </p>
+          <p className='py-4'>These are the technologies I've worked with</p>
+        </div>
+
+        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Html} alt='HTML icon' />
+            <p className='my-4'>HTML</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Css} alt='CSS icon' />
+            <p className='my-4'>CSS</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img
+              className='w-20 mx-auto'
+              src={JavaScript}
+              alt='JAVASCRIPT icon'
+            />
+            <p className='my-4'>JAVASCRIPT</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={eact} alt='REACT icon' />
+            <p className='my-4'>REACT</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Github} alt='GITHUB icon' />
+            <p className='my-4'>GITHUB</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Git} alt='GIT icon' />
+            <p className='my-4'>GIT</p>
           </div>
 
-          <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Html} alt="HTML icon" />
-                  <p className='my-4'>HTML</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Css} alt="CSS icon" />
-                  <p className='my-4'>CSS</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={JavaScript} alt="JAVASCRIPT icon" />
-                  <p className='my-4'>JAVASCRIPT</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={eact} alt="REACT icon" />
-                  <p className='my-4'>REACT</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Github} alt="GITHUB icon" />
-                  <p className='my-4'>GITHUB</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Git} alt="GIT icon" />
-                  <p className='my-4'>GIT</p>
-              </div>
-             
-           
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Tailwind} alt="TAILWIND icon" />
-                  <p className='my-4'>TAILWIND</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Bootstrap} alt="BOOTSTRAP icon" />
-                  <p className='my-4'>BOOTSTRAP</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Sass} alt="SASS icon" />
-                  <p className='my-4'>SASS</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Canva} alt="CANVA icon" />
-                  <p className='my-4'>CANVA</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Node} alt="NODE icon" />
-                  <p className='my-4'>NODE.JS</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Express} alt="EXPRESS icon" />
-                  <p className='my-4'>EXPRESS.JS</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Mongo} alt="MONGO icon" />
-                  <p className='my-4'>MONGO.DB</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Postman} alt = 'POSTMAN icon'
-/>
-                  <p className='my-4'>POSTMAN</p>
-              </div>
-            
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Tailwind} alt='TAILWIND icon' />
+            <p className='my-4'>TAILWIND</p>
           </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img
+              className='w-20 mx-auto'
+              src={Bootstrap}
+              alt='BOOTSTRAP icon'
+            />
+            <p className='my-4'>BOOTSTRAP</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Sass} alt='SASS icon' />
+            <p className='my-4'>SASS</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Canva} alt='CANVA icon' />
+            <p className='my-4'>CANVA</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Node} alt='NODE icon' />
+            <p className='my-4'>NODE.JS</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Express} alt='EXPRESS icon' />
+            <p className='my-4'>EXPRESS.JS</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Mongo} alt='MONGO icon' />
+            <p className='my-4'>MONGO.DB</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Postman} alt='POSTMAN icon' />
+            <p className='my-4'>POSTMAN</p>
+          </div>
+          <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+            <img className='w-20 mx-auto' src={Insomia} alt='POSTMAN icon' />
+            <p className='my-4'>INSOMNIA</p>
+          </div>
+        </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
 export default Skills
